d46fa640b32570e37715b3a91ddfa45832e5cb74
Paola L <paolalarrazabal@gmail.com>
2020-01-14 12:09:22 -0500

    removed db ps

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "TT11lala:*",
+        password: "",
         database: "burgers_db"
     });
 };
